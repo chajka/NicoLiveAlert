a5caacf5716dfa191a4c013c34539c01a0899bd5
Я Чайка <mellanie335@gmail.com>
2012-03-11 15:26:30 +0900

    Change Application Delegate class/file name to NicoLiveAlert

---
+++ NicoLiveAlert/NicoLiveAlert.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/11/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NicoLiveAlert.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end

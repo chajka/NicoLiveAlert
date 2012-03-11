6ade8ecfadd7ecf02dd72929e676dde3dc1ff007
Я Чайка <mellanie335@gmail.com>
2012-03-12 07:15:15 +0900

    Test Initialize and set/get value

--- NicoLiveAlertTests/Keychain/KCSUserTsts.m
+++ NicoLiveAlertTests/Keychain/KCSUserTsts.m
@@ -11,6 +11,9 @@
 
 #define USERNAME	@"chajka.niconico@gmail.com"
 #define PASSWORD	@"somepassword"
+#define SERVER		@"secure.nicovideo.jp"
+#define SERVPATH	@""
+#define SECDOMAIN	@"https"
 
 @implementation KCSUserTsts
 
@@ -28,21 +31,81 @@
     [super tearDown];
 }// end - (void) tearDown
 
-- (void) initializeKCSUserTest
+- (void) testInitializeKCSUser
 {
+		// allocation test
 	KCSUser *user = [[KCSUser alloc] init];
+
+		// check initial value
 	STAssertNotNil(user, @"Genarate KCSUser instance failed");
 	NSString *username = [user account];
-	STAssertNil(username, @"Fail initial accout is not NILL");
+	STAssertNil(username, @"Initial accout is not NILL");
 	NSString *password = [user password];
-	STAssertNil(password, @"Fail initial password is not NILL");
+	STAssertNil(password, @"Initial password is not NILL");
+	SecKeychainRef keyChain = [user keyChain];
+	STAssertNil((__bridge_transfer id)keyChain, @"Initial keyChain is not NILL");
+	SecKeychainItemRef itemRef = [user keyChainItem];
+	STAssertNil((__bridge_transfer id)itemRef, @"Inital keyChainItem is not NILL");
+	
+		// set get value check
 	[user setAccount:USERNAME];
 	username = [user account];
-	STAssertEquals(USERNAME, username, @"Fail set and get user name is not match");
+	STAssertEquals(USERNAME, username, @"Set and get user name is not match");
 	[user setPassword:PASSWORD];
 	password = [user password];
-	STAssertEquals(PASSWORD, password, @"Fail set and get password is not match");	
-}// end - (void) initializeTest
+	STAssertEquals(PASSWORD, password, @"Set and get password is not match");	
+}// end - (void) testInitializeKCSUser
+
+- (void) testInitializeKCSInternetUser
+{
+		// allocation test
+	KCSInternetUser *user = [[KCSInternetUser alloc] init];
+
+		// check initial value (inherited)
+	STAssertNotNil(user, @"Generate KCSInternetUser instance failed");
+	NSString *username = [user account];
+	STAssertNil(username, @"Initial accout is not NILL");
+	NSString *password = [user password];
+	STAssertNil(password, @"Initial password is not NILL");
+
+		// check inital value (class sepecific)
+	NSString *server = [user serverName];
+	STAssertNil(server, @"Initial server name is not NILL");
+	NSString *path = [user serverPath];
+	STAssertNil(path, @"Initial server path is not NILL");
+	NSString *domain = [user securityDomain];
+	STAssertNil(domain, @"Initial domain is not NILL");
+	SecProtocolType protocol = [user protocol];
+	STAssertTrue((protocol == kSecProtocolTypeAny), @"Initial protocol is not kSecProtocolTypeAny");
+	SecAuthenticationType auth = [user authType];
+	STAssertTrue((auth == kSecAuthenticationTypeAny), @"Initial authentication type is not kSecAuthenticationTypeAny");
+
+		// set get value check (inherited)
+	[user setAccount:USERNAME];
+	username = [user account];
+	STAssertEquals(USERNAME, username, @"Set and get user name is not match");
+	[user setPassword:PASSWORD];
+	password = [user password];
+	STAssertEquals(PASSWORD, password, @"Set and get password is not match");
+
+		// set get value check (class specific)
+	[user setServerName:SERVER];
+	server = [user serverName];
+	STAssertEquals(server, SERVER, @"Set and get server name is not match");
+	[user setServerPath:SERVPATH];
+	path = [user serverPath];
+	STAssertEquals(path, SERVPATH, @"Set and get server path is not match");
+	[user setSecurityDomain:SERVER];
+	domain = [user securityDomain];
+	STAssertEquals(domain, SERVER, @"Set and get sercurity domain is not match");
+	[user setProtocol:kSecProtocolTypeHTTP];
+	protocol = [user protocol];
+	STAssertTrue((protocol == kSecProtocolTypeHTTP), @"Set and Get protocol is not match");
+	[user setAuthType:kSecAuthenticationTypeHTMLForm];
+	auth = [user authType];
+	STAssertTrue((auth == kSecAuthenticationTypeHTMLForm), @"Set and Get authentication type is not match");
+
+}// end - (void) testInitializeKCSInternetUser
 
 - (void)testExample
 {

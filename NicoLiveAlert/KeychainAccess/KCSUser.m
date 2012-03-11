3e60bbd085e22be7ff038159f467daa06696d5e5
Я Чайка <mellanie335@gmail.com>
2012-03-11 19:07:59 +0900

    Add empty class KCSUser into project

---
+++ NicoLiveAlert/KeychainAccess/KCSUser.m
@@ -0,0 +1,13 @@
+//
+//  KCSUser.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/11/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "KCSUser.h"
+
+@implementation KCSUser
+
+@end

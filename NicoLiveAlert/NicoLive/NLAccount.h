dc8b0e8d5e759b69b92e178ec0b5436e61887d3a
Я Чайка <mellanie335@gmail.com>
2012-03-22 22:11:28 +0900

    Add class NLAccount with property member variables

---
+++ NicoLiveAlert/NicoLive/NLAccount.h
@@ -0,0 +1,24 @@
+//
+//  NLAccount.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/22/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLAccount : NSObject {
+	NSString			*mailaddr;
+	NSString			*password;
+	NSString			*username;
+	NSNumber			*userid;
+	NSMutableDictionary	*channels;
+}
+@property (copy, readwrite)	NSString			*mailaddr;
+@property (copy, readwrite)	NSString			*password;
+@property (copy, readwrite)	NSString			*username;
+@property (copy, readwrite)	NSNumber			*userid;
+@property (copy, readwrite)	NSMutableDictionary	*channels;
+
+@end
---
+++ NicoLiveAlert/NicoLive/NLAccount.m
@@ -0,0 +1,18 @@
+//
+//  NLAccount.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/22/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLAccount.h"
+
+@implementation NLAccount
+@synthesize mailaddr;
+@synthesize password;
+@synthesize username;
+@synthesize userid;
+@synthesize channels;
+
+@end

12932ceb7da78ba61d8fb13d95285011292b40fa
Я Чайка <mellanie335@gmail.com>
2012-03-24 00:21:58 +0900

    Implement main methods not test yet

--- NicoLiveAlert/Network/HTTPConnection.h
+++ NicoLiveAlert/Network/HTTPConnection.h
@@ -8,10 +8,95 @@
 
 #import <Foundation/Foundation.h>
 
+
+
 @interface HTTPConnection : NSObject {
+@protected
 	NSURL			*url;
 	NSString		*path;
 	NSDictionary	*params;
+	NSURLResponse	*response;
+	NSTimeInterval	timeout;
 }
+@property (copy, readwrite)		NSURL			*url;
+@property (copy, readwrite)		NSString		*path;
+@property (copy, readwrite)		NSDictionary	*params;
+@property (readonly)			NSURLResponse	*response;
+@property (assign, readwrite)	NSTimeInterval	timeout;
+	// class method
+/*!
+	@method HTTPSource:
+	@abstract Return contents of requested URL by NSString.
+	@param URL of request.
+	@param resoponse from server.
+	@result html data by string format.
+*/
++ (NSString *) HTTPSource:(NSURL *)url response:(NSURLResponse **)resp;
 
+/*!
+	@method HTTPSource:
+	@abstract Return contents of requested URL by NSData.
+	@param URL of request.
+	@param resoponse from server.
+	@result html data by binary format.
+ */
++ (NSData *) HTTPData:(NSURL *)url response:(NSURLResponse **)resp;
+	// constructor
+/*!
+	@method init
+	@abstract create HTTPConnection object and clear all member variable.
+	@result new clean HTTPConnection object.
+*/
+- (id) init;
+
+/*!
+	@method initWithURL:withParams:
+	@abstract create HTTPConnection object with URL and query paramerters.
+	@param URL of access this object.
+	@param query parameters by key-value pair dictionary or NULL.
+	@result new HTTPConnection object with URL. 
+*/
+- (id) initWithURL:(NSURL *)url_ withParams:(NSDictionary *)param;
+	// instance methods
+/*!
+	@method clearResponse
+	@abstract clear readonly response object.
+	@discussion this method might be call after post/get method.
+	because response store last value.
+*/
+- (void) clearResponse;
+
+/*!
+	@method stringByGet
+	@abstract get contents of URL by string format with own parameters.
+ */
+- (NSString *) stringByGet;
+
+/*!
+	@method dataByGet
+	@abstract get contents of URL by binary format with own parameters.
+ */
+- (NSData *) dataByGet;
+
+/*!
+	@method stringByPost:
+	@abstract get contents of URL by string format with posted own parameters.
+	@param error result.
+ */
+- (NSString *) stringByPost:(NSError **)error;
+
+/*!
+	@method dataByPost:
+	@abstract get contents of URL by binary format with posted own parameters.
+	@param error result.
+*/
+- (NSData *) dataByPost:(NSError **)error;
+
+/*!
+	@method httpDataAsyncWithdelegate:
+	@abstract send HTTP request with async data transfer.
+	@param delegate object for data recieve. if nil, it dosen’t work.
+	@result NSURLConnection object of this connection;
+ */
+- (NSURLConnection *) httpDataAsyncWithdelegate:(id)target;
 @end
--- NicoLiveAlert/Network/HTTPConnection.m
+++ NicoLiveAlert/Network/HTTPConnection.m
@@ -8,6 +8,255 @@
 
 #import "HTTPConnection.h"
 
-@implementation HTTPConnection
+	// for HTTP connection method literal
+#define RequestMethodPost	@"POST"
+#define RequestMethodGet	@"GET"
+	//	for creating query literal
+#define ParamConcatFormat	@"%@=%@"
+#define ParamsConcatSymbol	@"&"
+#define QueryConcatFormat	@"?%@"
 
+const NSTimeInterval defaultTimeout = 30; // second
+
+#pragma mark private methods
+@interface HTTPConnection ()
+/*!
+	@method buildParam
+	@abstract clear readonly response object.
+	@discussion this method might be call after post/get method.
+	because response store last value.
+	@result new HTTPConnection object with URL. 
+ */
+- (NSString *) buildParam;
+
+/*!
+	@method stringByPost:
+	@abstract get contents of URL by string format with posted own parameters.
+	@param error result.
+	@result new HTTPConnection object with URL. 
+ */
+- (NSData *) post:(NSError **)err;
+@end
+
+@implementation HTTPConnection
+@synthesize url;
+@synthesize path;
+@synthesize params;
+@synthesize response;
+@synthesize timeout;
+
+#pragma mark class methods
++ (NSString *) HTTPSource:(NSURL *)url response:(NSURLResponse **)resp
+{		// create detamine encoding constant array 
+	NSArray *encodings = [NSArray arrayWithObjects:
+			  [NSNumber numberWithUnsignedInt:NSUTF8StringEncoding],
+			  [NSNumber numberWithUnsignedInt:NSShiftJISStringEncoding], 
+			  [NSNumber numberWithUnsignedInt:NSJapaneseEUCStringEncoding], 
+			  [NSNumber numberWithUnsignedInt:NSISO2022JPStringEncoding], 
+			  [NSNumber numberWithUnsignedInt:NSUnicodeStringEncoding], 
+			  [NSNumber numberWithUnsignedInt:NSASCIIStringEncoding], nil];
+
+		// check have data
+	NSData *receivedData = [HTTPConnection HTTPData:url response:resp];
+	if (receivedData == NULL)
+		return NULL;
+
+		// datamine encoding
+	NSString *data_str = NULL;
+	for (NSNumber *enc in encodings)
+	{
+#if __has_feature(objc_arc)
+		data_str = [[NSString alloc] initWithData:receivedData encoding:[enc unsignedIntValue]];
+#else
+		data_str = [[[NSString alloc] initWithData:receivedData encoding:[enc unsignedIntValue]] autorelease];
+#endif
+		if (data_str!=nil)
+			break;
+	}// end for each encodings
+	return data_str;
+}// end + (NSString *) HTTPSource:(NSURL *)url
+
++ (NSData *) HTTPData:(NSURL *)url response:(NSURLResponse **)resp
+{
+	NSURLRequest *request = [NSURLRequest requestWithURL:url];
+	NSError *error = nil;
+	NSURLResponse *re;
+	NSData *receivedData = [NSURLConnection sendSynchronousRequest:request
+												 returningResponse:&re
+															 error:&error];
+	*resp = re;
+	
+		// error check
+	if ([error code] != noErr)
+		return NULL;
+	else
+		return receivedData;
+}// end + (NSString *) HTTPSource:(NSURL *)url
+
+#pragma mark -
+#pragma mark construct/destruct
+- (id) init
+{
+	self = [super init];
+	if (self)
+	{
+		url = NULL;
+		path = NULL;
+		params = NULL;
+		response = NULL;
+		timeout = defaultTimeout;
+	}// end if self
+	return self;
+}// end - (id) init
+
+- (id) initWithURL:(NSURL *)url_ withParams:(NSDictionary *)param
+{
+	self = [super init];
+	if (self)
+	{
+		url = url_;
+		path = NULL;
+		params = param;
+		response = NULL;
+		timeout = defaultTimeout;
+	}// end if self
+	return self;
+}// end - (id) initWithURL:(NSURL *)url_ withParams:(NSDictionary *)param
+
+- (void) dealloc
+{
+#if __has_feature(objc_arc) == 0
+    [url release];
+	[path release];
+	[params release];
+	[response release];
+	[super dealloc];
+#endif
+}// end - (void) dealloc
+#pragma mark -
+#pragma mark instance methods
+- (void) clearResponse
+{
+#if __has_feature(objc_arc) == 0
+	[response autorelease];
+#endif
+	response = NULL;
+}// end - (void) clearResponse
+
+- (NSString *) stringByGet
+{
+	NSURL *queryURL = NULL;
+	NSString *query = NULL;
+	if (params != NULL)
+		query = [NSString stringWithFormat:QueryConcatFormat, [self buildParam]];
+	queryURL = [NSURL URLWithString:[NSString stringWithFormat:QueryConcatFormat, [url absoluteString], query]];
+	NSURLResponse *resp = NULL;
+	NSString *string = NULL;
+	string = [HTTPConnection HTTPSource:queryURL response:&resp];
+	response = [resp copy];
+	
+	return string;
+}// end - (NSString *) stringByGet:(NSError **)error
+
+- (NSData *) dataByGet
+{
+	NSURL *queryURL = NULL;
+	NSString *query = NULL;
+	if (params != NULL)
+		query = [NSString stringWithFormat:QueryConcatFormat, [self buildParam]];
+	queryURL = [NSURL URLWithString:[NSString stringWithFormat:QueryConcatFormat, [url absoluteString], query]];
+	NSURLResponse *resp;
+	NSData *data = [HTTPConnection HTTPData:queryURL response:&resp];
+	response = [resp copy];
+	return data;
+}// end - (NSData *) dataByGet:(NSError **)error
+
+- (NSString *) stringByPost:(NSError **)error
+{
+	NSData *receivedData = [self post:error];
+	if ([*error code] != noErr)
+		return NULL;
+		// create detamine encoding constant array 
+	NSArray *encodings = [NSArray arrayWithObjects:
+						  [NSNumber numberWithUnsignedInt:NSUTF8StringEncoding],
+						  [NSNumber numberWithUnsignedInt:NSShiftJISStringEncoding], 
+						  [NSNumber numberWithUnsignedInt:NSJapaneseEUCStringEncoding], 
+						  [NSNumber numberWithUnsignedInt:NSISO2022JPStringEncoding], 
+						  [NSNumber numberWithUnsignedInt:NSUnicodeStringEncoding], 
+						  [NSNumber numberWithUnsignedInt:NSASCIIStringEncoding], nil];
+
+	// datamine encoding
+	NSString *data_str = NULL;
+	for (NSNumber *enc in encodings)
+	{
+#if __has_feature(objc_arc)
+		data_str = [[NSString alloc] initWithData:receivedData encoding:[enc unsignedIntValue]];
+#else
+		data_str = [[[NSString alloc] initWithData:receivedData encoding:[enc unsignedIntValue]] autorelease];
+#endif
+		if (data_str!=nil)
+			break;
+	}// end for each encodings
+
+	return data_str;
+}// end - (NSString *) stringByPost:(NSError **)error
+
+- (NSData *) dataByPost:(NSError **)error
+{
+	return [self post:error];
+}// end - (NSData *) dataByPost:(NSError **)error
+
+- (NSURLConnection *) httpDataAsyncWithdelegate:(id)target
+{
+	if (target == NULL)
+		return NULL;
+
+	NSURLRequest *request;
+	request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy
+						   timeoutInterval:timeout];
+	NSURLConnection *connection;
+	connection = [[NSURLConnection alloc] initWithRequest:request delegate:target];
+#if __has_feature(objc_arc) == 0
+	[connection autorelease];
+#endif
+	
+	return connection;
+}// end - (NSURLConnection *) httpDataAsync:(NSURL *)url delegate:(id)target
+
+#pragma mark private methods
+- (NSData*) post:(NSError **)err;
+{
+		// create request
+	NSMutableURLRequest* urlRequest = [[NSMutableURLRequest alloc]initWithURL:url];
+		// create psot body
+	NSString *message = [self buildParam];
+	NSURLResponse *resp = NULL;
+	NSData *httpBody = [message dataUsingEncoding:NSUTF8StringEncoding];
+	[urlRequest setHTTPMethod:RequestMethodPost];
+	[urlRequest setHTTPBody:httpBody];
+	NSData* result = [NSURLConnection sendSynchronousRequest:urlRequest
+										   returningResponse:&resp
+													   error:err];
+	response = [resp copy];
+#if __has_feature(objc_arc) == 0
+	[urlRequest autorelease];
+#endif
+	if ([*err code] != noErr)
+		return NULL;
+	else
+		return result;
+}// end - (NSData*) post
+
+- (NSString *) buildParam
+{
+	NSString *param = NULL;
+	NSMutableArray *messages = [NSMutableArray array];
+	for (NSString *key in [params allKeys])
+	{
+		[messages addObject:[NSString stringWithFormat:ParamConcatFormat, key, [params objectForKey:key]]];
+	}// end for
+	param = [[messages componentsJoinedByString:ParamsConcatSymbol] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+
+	return param;
+}// end - (NSString *) buildParam
 @end

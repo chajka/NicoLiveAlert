5374022a7e9741e65bf81a0f0c7fee996254b013
Я Чайка <mellanie335@gmail.com>
2012-03-24 14:07:46 +0900

    Add empty class NLStatusIcon.

---
+++ NicoLiveAlert/NicoLive/NLStatusbarIcon.h
@@ -0,0 +1,13 @@
+//
+//  NLStatusbarIcon.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/24/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLStatusbarIcon : NSObject
+
+@end
---
+++ NicoLiveAlert/NicoLive/NLStatusbarIcon.m
@@ -0,0 +1,13 @@
+//
+//  NLStatusbarIcon.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/24/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLStatusbarIcon.h"
+
+@implementation NLStatusbarIcon
+
+@end

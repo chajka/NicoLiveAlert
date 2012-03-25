ec0dbbd0d7b548c5852b34860c7d0576a4bb692b
Я Чайка <mellanie335@gmail.com>
2012-03-25 16:25:40 +0900

    Add empty class NLUsers into project

---
+++ NicoLiveAlert/NicoLive/NLUsers.h
@@ -0,0 +1,13 @@
+//
+//  NLUsers.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/25/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLUsers : NSObject
+
+@end
---
+++ NicoLiveAlert/NicoLive/NLUsers.m
@@ -0,0 +1,13 @@
+//
+//  NLUsers.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/25/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLUsers.h"
+
+@implementation NLUsers
+
+@end

ca2d3a7aa073f4a96f2f759460f6f2ff684e248a
Я Чайка <mellanie335@gmail.com>
2012-03-25 21:23:37 +0900

    Add unit test class NLAccountTests into project

---
+++ NicoLiveAlertTests/NicoLive/NLAccountTests.h
@@ -0,0 +1,14 @@
+//
+//  NLAccountTests.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/25/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "NLAccount.h"
+
+@interface NLAccountTests : SenTestCase
+
+@end
---
+++ NicoLiveAlertTests/NicoLive/NLAccountTests.m
@@ -0,0 +1,13 @@
+//
+//  NLAccountTests.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/25/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLAccountTests.h"
+
+@implementation NLAccountTests
+
+@end

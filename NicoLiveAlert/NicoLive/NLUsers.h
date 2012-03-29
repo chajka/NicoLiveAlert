b18b39502e414b830497a3f876ca62406df82e84
Я Чайка <mellanie335@gmail.com>
2012-03-29 12:31:31 +0900

    Implement method of class NLUsers and write document comment. It didn't unit test yet.

--- NicoLiveAlert/NicoLive/NLUsers.h
+++ NicoLiveAlert/NicoLive/NLUsers.h
@@ -11,16 +11,59 @@
 #import "KCSUser.h"
 #import "NLAccount.h"
 
+/*!
+	@header NLUsers.h
+
+	This header file describes the user management class for NicoLiveAlert.
+
+	this class for working
+	• correct internet password for nicovideo
+	• calcurate wathclist from active and deactive users 
+	• cleate user status menu
+*/
+
 @interface NLUsers : NSObject {
-	NSMutableArray		*enabledUsers;
-	NSMutableArray		*disabledUsers;
-	NSDictionary		*accounts;
-	NSMutableDictionary	*originalWatchList;
+	NSNumber			*active;
+	NSNumber			*deactive;
+	NSMutableDictionary	*usersState;
+	NSMutableDictionary	*accounts;
+	NSDictionary		*originalWatchList;
 	NSMutableDictionary	*watchlist;
+	NSMenu				*usersMenu;
 }
-@property (readonly)	NSDictionary	*watchlist;
+@property (readonly) NSMutableDictionary	*watchlist;
+@property (readonly) NSMenu					*usersMenu;
 
 #pragma mark constructor / destructor
+/*!
+	@method initWithActiveUsers:andManualWatchList:
+	@abstract create nicolive account list with active usrs and manual wathlist.
+	@param array of mailaddress (login account) for watch joind community.
+	@param manually added watchlist with autoOpen flag.
+	@result active and deactive user management object
+*/
 - (id) initWithActiveUsers:(NSArray *)users andManualWatchList:(NSDictionary *)manualWatchList;
+
+#pragma mark -
+#pragma mark user management
+/*!
+	@method addUser:andPassword:
+	@abstract add user account by maila ddress with password.
+	This user hold by NLAccount instance in reciever.
+	this account store to keychain for nicovideo.
+	@result status for store to keychain.
+	Or 1 is failed to login nicovideo.
+ */
+- (OSStatus) addUser:(NSString *)useraccount withPassword:(NSString *)userpassword;
+
+#pragma mark -
+#pragma mark menu management
+/*!
+ @method toggleUserState:
+ @abstract switch sender to watch or unwatch.
+ result status shown by users menuItem's check mark.
+ It effect to reconstruct content of watchlist of reciever.
+*/
+- (void) toggleUserState:(id)sender;
 
 @end
--- NicoLiveAlert/NicoLive/NLUsers.m
+++ NicoLiveAlert/NicoLive/NLUsers.m
@@ -9,12 +9,18 @@
 #import "NLUsers.h"
 
 @interface NLUsers ()
-- (NSMutableDictionary *) makeAccounts;
-- (void) makeCurrentWatchlist:(NSArray *)users;
+- (NSMutableDictionary *) makeAccounts:(NSArray *)users;
+- (void) makeCurrentWatchlist;
+/*!
+ @method creteUserStateMenu
+ @abstract
+ */
+- (void) creteUserStateMenu;
 @end
 
 @implementation NLUsers
 @synthesize watchlist;
+@synthesize usersMenu;
 
 #pragma mark constructor / destructor
 - (id) initWithActiveUsers:(NSArray *)users andManualWatchList:(NSDictionary *)manualWatchList
@@ -22,12 +28,18 @@
 	self = [super init];
 	if (self)
 	{
-		enabledUsers = [NSMutableArray arrayWithArray:users];
-		disabledUsers = [NSMutableArray array];
-		accounts = [NSDictionary dictionaryWithDictionary:[self makeAccounts]];
-		originalWatchList = [manualWatchList copy];
-		watchlist = [NSMutableDictionary dictionary];
-		[self makeCurrentWatchlist:users];
+		active = [[NSNumber alloc] initWithBool:YES];
+		deactive = [[NSNumber alloc] initWithBool:NO];
+		usersState = [[NSMutableDictionary alloc] init];
+		accounts = [[NSMutableDictionary alloc] initWithDictionary:[self makeAccounts:users]];
+		originalWatchList = manualWatchList;
+		watchlist = [[NSMutableDictionary alloc] init];
+#if __has_feature(objc_arc) == 0
+		[watchlist retain];
+#endif
+		[self makeCurrentWatchlist];
+		usersMenu = NULL;
+		[self creteUserStateMenu];
 	}
 	return self;
 }// end - (id) initWithActiveUsers:(NSArray *)users andManualWatchList:(NSDictionary *)manualWatchList
@@ -35,32 +47,136 @@
 - (void) dealloc
 {
 #if __has_feature(objc_arc) == 0
+	if (active != NULL) {				[active release]; }
+	if (deactive != NULL) {				[deactive release]; }
+	if (usersState != NULL) {			[usersState release]; }
+	if (accounts != NULL) {				[accounts release]; }
 	if (originalWatchList != NULL) {	[originalWatchList release]; }
+	if (watchlist != NULL) {			[watchlist release]; }
+	if (usersMenu != NULL)
+	{
+		for (NSMenuItem *item in [usersMenu itemArray])
+			[usersMenu removeItem:item];
+		// end foreach delete users menuitem.
+		[usersMenu release];
+	}// end if cleanup usersMenu
 	[super dealloc];
 #endif
 }// end - (void) dealloc
 
 #pragma mark constructor support
-- (NSMutableDictionary *) makeAccounts
+- (NSMutableDictionary *) makeAccounts:(NSArray *)users
 {
 	NSArray *usesArray = [KCSInternetUser usersOfAccountsForServer:NICOLOGINSERVER path:NICOLOGINPATH forAuthType:kSecAuthenticationTypeAny inKeychain:systemDefaultKeychain];
 	if ((usesArray == NULL) || ([usesArray count] == 0))
 		return NULL;
 
 	NSMutableDictionary *usersDict = [NSMutableDictionary dictionary];
+	NLAccount *account;
 	for (KCSInternetUser *user in usesArray)
-		[usersDict setValue:user forKey:[user account]];
+	{
+		account = [[NLAccount alloc] initWithAccount:[user account] andPassword:[user password]];
+#if __has_feature(objc_arc) == 0
+		[account autorelease];
+#endif
+		[usersDict setValue:account forKey:[account username]];
+		if ([users containsObject:[user account]] == YES)
+			[usersState setValue:active forKey:[account username]];
+		else
+			[usersState setValue:deactive forKey:[account username]];
+		// end if is set userState)
+	}// end for
 
 	return usersDict;
 }// end - (NSDictionary *) makeAccounts
 
-- (void) makeCurrentWatchlist:(NSArray *)users
+- (void) makeCurrentWatchlist
 {
-	for (NSString *currentUser in users)
-		[watchlist addEntriesFromDictionary:[[accounts valueForKey:currentUser] channels]];
-	// end foreach active users.
+	[watchlist removeAllObjects];
+
+	for (NSString *currentUser in [usersState allKeys])
+	{
+		if ([[usersState valueForKey:currentUser] boolValue] == YES)
+			[watchlist addEntriesFromDictionary:[[accounts valueForKey:currentUser] channels]];
+	}// end foreach active users.
 
 	[watchlist addEntriesFromDictionary:originalWatchList];
 }// end - (void) makeCurrentWatchlist:(NSArray *)users
 
+#pragma mark -
+#pragma mark user management
+- (OSStatus) addUser:(NSString *)useraccount withPassword:(NSString *)userpassword
+{
+	OSStatus error = 1;
+		// create NLAccount instance
+	NLAccount *user = [[NLAccount alloc] initWithAccount:useraccount andPassword:userpassword];
+	if (user == NULL)
+		return error;
+	// end if user was logined.
+		// store account
+	[accounts setValue:user forKey:[user username]];
+	[usersState setValue:deactive forKey:[user username]];
+#if __has_feature(objc_arc) == 0
+	[user release];
+#endif
+		// update current watch list
+	[self makeCurrentWatchlist];
+
+		// add to keychain this user
+	KCSInternetUser *keychainOfUser =
+	[[KCSInternetUser alloc] initWithAccount:useraccount andPassword:userpassword];
+	[keychainOfUser setServerName:NICOLOGINSERVER];
+	[keychainOfUser setServerPath:NICOLOGINPATH];
+	[keychainOfUser setAuthType:kSecAuthenticationTypeHTMLForm];
+	[keychainOfUser setProtocol:kSecProtocolTypeHTTPS];
+	[keychainOfUser setKeychainName:[NSString stringWithFormat:NICOKEYCHAINNAMEFORMAT,NICOLOGINSERVER, useraccount]];
+	[keychainOfUser setKeychainKind:NICOKEYCHAINLABEL];
+	if ([keychainOfUser addTokeychain] == YES)
+		error = noErr;
+	else
+		error = [keychainOfUser status];
+
+	return error;
+}// end - (OSStatus) addUser:(NSString *)useraccount andPassword:(NSString *)userpassword
+#pragma mark -
+#pragma mark menu management
+- (void) creteUserStateMenu
+{
+	usersMenu = [[NSMenu alloc] initWithTitle:@""];
+
+	NSMenuItem *userItem;
+	NSImage *onStateImg = [NSImage imageNamed:@"NSMenuCheckmark"];
+	for (NSString *user in [usersState allKeys])
+	{
+		userItem = [[NSMenuItem alloc] initWithTitle:user action:@selector(toggleUserState:) keyEquivalent:@""];
+		[userItem setOnStateImage:onStateImg];
+		[userItem setOffStateImage:NULL];
+		if ([[usersState valueForKey:user] isEqual:active] == YES)
+			[userItem setState:NSOnState];
+		else
+			[userItem setState:NSOffState];
+		// end if set user's state
+		[userItem setEnabled:YES];
+		[usersMenu addItem:userItem];
+#if __has_feature(objc_arc) == 0
+		[userItem autorelease];
+#endif
+	}// end foreach user
+}// end - (NSMenu *) creteUserStateMenu
+
+- (void) toggleUserState:(id)sender
+{
+	NSMenuItem *item = (NSMenuItem *)sender;
+	if ([item state] == NSOnState)
+	{
+		[item setState:NSOffState];
+		[usersState setValue:deactive forKey:[item title]];
+	}
+	else
+	{
+		[item setState:NSOnState];
+		[usersState setValue:active forKey:[item title]];
+	}// end if
+	[self makeCurrentWatchlist];
+}// end - (IBAction) toggleUserState:(id)sender
 @end

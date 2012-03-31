d2bb6473db438ad3e0193126752497c615e8a14d
Я Чайка <mellanie335@gmail.com>
2012-03-31 21:24:04 +0900

    Add empty class Socket Connection

---
+++ NicoLiveAlert/Network/SocketConnection.h
@@ -0,0 +1,13 @@
+//
+//  SocketConnection.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/31/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketConnection : NSObject
+
+@end
---
+++ NicoLiveAlert/Network/SocketConnection.m
@@ -0,0 +1,13 @@
+//
+//  SocketConnection.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/31/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "SocketConnection.h"
+
+@implementation SocketConnection
+
+@end

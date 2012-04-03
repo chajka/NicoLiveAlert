077b5baab171739d85c10d36ca0bbfb50eb7e1a4
Я Чайка <mellanie335@gmail.com>
2012-04-04 01:29:03 +0900

    Add empty class NLProgramList

---
+++ NicoLiveAlert/NicoLive/NLProgramList.h
@@ -0,0 +1,13 @@
+//
+//  NLProgramList.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/4/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLProgramList : NSObject
+
+@end
---
+++ NicoLiveAlert/NicoLive/NLProgramList.m
@@ -0,0 +1,13 @@
+//
+//  NLProgramList.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/4/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLProgramList.h"
+
+@implementation NLProgramList
+
+@end

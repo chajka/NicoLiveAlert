4856ccab1e7c5ed0dec53918c28373ef844ed1bb
Я Чайка <mellanie335@gmail.com>
2012-04-03 14:23:15 +0900

    Update state image
    Refine alpha
    
    Signed-off-by: Я Чайка <mellanie335@gmail.com>

---
+++ NicoLiveAlert/GUI/NLProgramMenuItem.h
@@ -0,0 +1,13 @@
+//
+//  NLProgramMenuItem.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/3/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLProgramMenuItem : NSMenuItem
+
+@end
---
+++ NicoLiveAlert/GUI/NLProgramMenuItem.m
@@ -0,0 +1,13 @@
+//
+//  NLProgramMenuItem.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/3/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLProgramMenuItem.h"
+
+@implementation NLProgramMenuItem
+
+@end

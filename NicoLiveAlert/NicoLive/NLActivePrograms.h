c0f6ec2df1033ed08ae2b143bd68409c2a2fa426
Я Чайка <mellanie335@gmail.com>
2012-04-12 09:46:41 +0900

    Add empty class NLActivePrograms.

---
+++ NicoLiveAlert/NicoLive/NLActivePrograms.h
@@ -0,0 +1,13 @@
+//
+//  NLActivePrograms.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/12/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLActivePrograms : NSObject
+
+@end
---
+++ NicoLiveAlert/NicoLive/NLActivePrograms.m
@@ -0,0 +1,13 @@
+//
+//  NLActivePrograms.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/12/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLActivePrograms.h"
+
+@implementation NLActivePrograms
+
+@end

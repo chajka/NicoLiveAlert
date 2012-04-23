596d6de4d703b5874d23144518e98272e460832f
Я Чайка <mellanie335@gmail.com>
2012-04-24 08:41:56 +0900

    Add empty class NicoLivePrefManager.

---
+++ NicoLiveAlert/ApplicationSupport/NicoLivePrefManager.h
@@ -0,0 +1,13 @@
+//
+//  NicoLivePrefManager.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/24/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NicoLivePrefManager : NSObject
+
+@end
---
+++ NicoLiveAlert/ApplicationSupport/NicoLivePrefManager.m
@@ -0,0 +1,13 @@
+//
+//  NicoLivePrefManager.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 4/24/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NicoLivePrefManager.h"
+
+@implementation NicoLivePrefManager
+
+@end

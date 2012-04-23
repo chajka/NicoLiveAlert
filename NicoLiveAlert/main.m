0c7561f680f0c5a5b1c22e7eee3c7d78f4fc6d2c
Я Чайка <mellanie335@gmail.com>
2012-04-23 20:18:00 +0900

    Exception catchable for debug.

--- NicoLiveAlert/main.m
+++ NicoLiveAlert/main.m
@@ -10,5 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	return NSApplicationMain(argc, (const char **)argv);
+	@try {
+		return NSApplicationMain(argc, (const char **)argv);
+	}
+	@catch (NSException *exception) {
+		NSLog(@"%@, %@, %@", [exception name], [exception reason], [exception userInfo]);
+	}
 }

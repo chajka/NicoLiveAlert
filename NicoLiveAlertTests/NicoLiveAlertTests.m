bbcc8d6fc84734ea4fb10284fdc1e43b2442ea4b
Я Чайка <mellanie335@gmail.com>
2012-05-05 11:17:44 +0900

    commit tests.

--- NicoLiveAlertTests/NicoLiveAlertTests.m
+++ NicoLiveAlertTests/NicoLiveAlertTests.m
@@ -26,7 +26,17 @@
 
 - (void)testExample
 {
-//    STFail(@"Unit tests are not implemented yet in NicoLiveAlertTests");
+	NSString *string1 = [NSString stringWithString:@"test1"];
+	NSString *string2 = [NSString stringWithString:@"test2"];
+	NSString *string3 = [NSString stringWithString:@"test3"];
+	NSString *string4 = [NSString stringWithString:@"test4"];
+	NSString *string5 = [NSString stringWithString:@"test5"];
+	NSString *string6 = [NSString stringWithString:@"test6"];
+	NSString *string7 = [NSString stringWithString:@"test2"];
+
+	NSArray *array = [NSMutableArray arrayWithObjects:string1, string2, string3, string4, string5, string6, nil];
+	BOOL found = [array containsObject:string7];
+	STAssertTrue(found, @"different object but contains same can findable");
 }
 
 @end

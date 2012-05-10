4ecb8332c9a7c5368f33dc1be7216b5c7f655390
Я Чайка <mellanie335@gmail.com>
2012-05-10 14:46:40 +0900

    Add empty class NLAccountInfo into project.

---
+++ NicoLiveAlert/NicoLive/NLAccountInfo.h
@@ -0,0 +1,13 @@
+//
+//  NLAccountInfo.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 5/10/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLAccountInfo : NSObject
+
+@end
---
+++ NicoLiveAlert/NicoLive/NLAccountInfo.m
@@ -0,0 +1,13 @@
+//
+//  NLAccountInfo.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 5/10/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLAccountInfo.h"
+
+@implementation NLAccountInfo
+
+@end

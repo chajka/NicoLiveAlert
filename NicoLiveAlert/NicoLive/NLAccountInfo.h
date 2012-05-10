c9d0a553a3ed6a6d7b617c375901bbb9958dd8a8
Я Чайка <mellanie335@gmail.com>
2012-05-10 23:15:32 +0900

    Implement constructor / destructor of class NLAccountInfo.

--- NicoLiveAlert/NicoLive/NLAccountInfo.h
+++ NicoLiveAlert/NicoLive/NLAccountInfo.h
@@ -7,7 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NLAccount.h"
 
-@interface NLAccountInfo : NSObject
+@interface NLAccountInfo : NSObject {
+	BOOL		enableAtStart;
+	NSNumber	*userid;
+	NSString	*nickname;
+	NSString	*mailaddr;
+}
+@property (assign, readonly) BOOL		enableAtStart;
+@property (copy, readonly) NSNumber	*userid;
+@property (copy, readonly) NSString	*nickname;
+@property (copy, readonly) NSString	*mailaddr;
+
+- (id) initWithAccountDict:(NSDictionary *)accountInfo;
+- (id) initWithAccount:(NLAccount *)account enableWatchAtNext:(BOOL)enable;
 
 @end
--- NicoLiveAlert/NicoLive/NLAccountInfo.m
+++ NicoLiveAlert/NicoLive/NLAccountInfo.m
@@ -7,7 +7,61 @@
 //
 
 #import "NLAccountInfo.h"
+#import "NicoLiveAlertDefinitions.h"
 
 @implementation NLAccountInfo
+@synthesize enableAtStart;
+@synthesize userid;
+@synthesize nickname;
+@synthesize mailaddr;
+
+- (id) initWithAccountDict:(NSDictionary *)accountInfo
+{
+	self = [super init];
+	if (self)
+	{
+		enableAtStart = [[accountInfo valueForKey:keyAccountWatchEnabled] boolValue];
+		userid = [[accountInfo valueForKey:keyAccountUserID] copy];
+		nickname = [[accountInfo valueForKey:keyAccountNickname] copy];
+		mailaddr = [[accountInfo valueForKey:keyAccountMailAddr] copy];
+#if __has_feature(objc_arc) == 0
+		[userid retain];
+		[nickname retain];
+		[mailaddr retain];
+#endif
+	}// end if
+
+	return self;
+}// end - (id) initWithAccountDict:(NSDictionary *)accountInfo
+
+- (id) initWithAccount:(NLAccount *)account enableWatchAtNext:(BOOL)enable;
+{
+	self = [super init];
+	if (self)
+	{
+		enableAtStart = enable;
+		userid = [[account userid] copy];
+		nickname = [[account username] copy];
+		mailaddr = [[account mailaddr] copy];
+#if __has_feature(objc_arc) == 0
+		[userid retain];
+		[nickname retain];
+		[mailaddr retain];
+#endif
+	}// end if
+
+	return self;
+}// end - (id) initWithAccount:(NLAccount *)account
+
+- (void) dealloc
+{
+#if __has_feature(objc_arc) == 0
+	if (userid != NULL)		[userid retain];
+	if (nickname != NULL)	[nickname retain];
+	if (mailaddr != NULL)	[mailaddr retain];
+
+	[super dealloc];
+#endif
+}
 
 @end

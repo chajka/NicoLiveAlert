9d6d27c6a3072a47375e19f851714e2bc291a8c1
Я Чайка <mellanie335@gmail.com>
2012-07-19 22:16:00 +0900

    Add class (was subclass of NSImageView) NLClickableImageView into project.

---
+++ NicoLiveAlert/GUI/NLClickableImageView.h
@@ -0,0 +1,13 @@
+//
+//  NLClickableImageView.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/19/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLClickableImageView : NSImageView
+
+@end
---
+++ NicoLiveAlert/GUI/NLClickableImageView.m
@@ -0,0 +1,28 @@
+//
+//  NLClickableImageView.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/19/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLClickableImageView.h"
+
+@implementation NLClickableImageView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end

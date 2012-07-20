e5f9aab483897a95bb54e86a5309d2b4b61e9a18
Я Чайка <mellanie335@gmail.com>
2012-07-20 10:53:19 +0900

    Create category NLProgram+Parsing for separate parse methods.

---
+++ NicoLiveAlert/NicoLive/NLProgram+Parsing.h
@@ -0,0 +1,13 @@
+//
+//  NLProgram+Parsing.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/20/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLProgram.h"
+
+@interface NLProgram (Parsing)
+
+@end
---
+++ NicoLiveAlert/NicoLive/NLProgram+Parsing.m
@@ -0,0 +1,13 @@
+//
+//  NLProgram+Parsing.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/20/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLProgram+Parsing.h"
+
+@implementation NLProgram (Parsing)
+
+@end

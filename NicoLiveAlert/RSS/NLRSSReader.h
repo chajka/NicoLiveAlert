347b8e6b55309ff1a7dae14f9b504e77ede0f2ba
Я Чайка <mellanie335@gmail.com>
2012-07-23 18:13:53 +0900

    Add empty class NLRSSReader.

---
+++ NicoLiveAlert/RSS/NLRSSReader.h
@@ -0,0 +1,13 @@
+//
+//  NLRSSReader.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/23/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLRSSReader : NSObject
+
+@end
---
+++ NicoLiveAlert/RSS/NLRSSReader.m
@@ -0,0 +1,13 @@
+//
+//  NLRSSReader.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/23/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "NLRSSReader.h"
+
+@implementation NLRSSReader
+
+@end

f9a4b82228f977cb351d8a2a3c025a0cba453da4
Я Чайка <mellanie335@gmail.com>
2014-03-25 12:48:14 +0900

    Add delegate methods.

--- NicoLiveAlert/Application/NicoLiveAlert.m
+++ NicoLiveAlert/Application/NicoLiveAlert.m
@@ -9,10 +9,36 @@
 #import "NicoLiveAlert.h"
 
 @implementation NicoLiveAlert
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+#pragma mark - synthesize properties
+#pragma mark - class method
+#pragma mark - constructor / destructor
+#pragma mark - override
+- (void) awakeFromNib
 {
-	// Insert code here to initialize your application
-}
+	
+}// end - (void) awakeFromNib
+
+#pragma mark - delegate
+- (void) applicationWillFinishLaunching:(NSNotification *)notification
+{
+	
+}// end - (void) applicationWillFinishLaunching:(NSNotification *)notification
+
+- (void) applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+		// Insert code here to initialize your application
+}// end - (void) applicationDidFinishLaunching:(NSNotification *)aNotification
+
+- (void) applicationWillTerminate:(NSNotification *)notification
+{
+	
+}// end - (void) applicationWillFinishLaunching:(NSNotification *)notification
+
+#pragma mark - instance method
+#pragma mark constructor
+#pragma mark - properties
+#pragma mark - messages
+#pragma mark - private
+#pragma mark - C functions
 
 @end

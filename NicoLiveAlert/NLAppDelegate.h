98325176585d5c81d0e76d0296f09bcf42391dcd
Я Чайка <mellanie335@gmail.com>
2014-03-25 12:16:40 +0900

    Initial Commit

---
+++ NicoLiveAlert/NLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NLAppDelegate.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/25/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ NicoLiveAlert/NLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  NLAppDelegate.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 3/25/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import "NLAppDelegate.h"
+
+@implementation NLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end

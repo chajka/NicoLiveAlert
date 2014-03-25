5e40d98ab268196519763b10632543f3d6b3ef35
Я Чайка <mellanie335@gmail.com>
2014-03-25 13:04:11 +0900

    Implement uncaught exception handler

--- NicoLiveAlert/Application/NicoLiveAlert.m
+++ NicoLiveAlert/Application/NicoLiveAlert.m
@@ -8,6 +8,18 @@
 
 #import "NicoLiveAlert.h"
 
+@interface NicoLiveAlert ()
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+static void uncaughtExceptionHandler(NSException *exception);
+#ifdef __cplusplus
+} //end extern "C"
+#endif
+
+@end
+
 @implementation NicoLiveAlert
 #pragma mark - synthesize properties
 #pragma mark - class method
@@ -15,7 +27,7 @@
 #pragma mark - override
 - (void) awakeFromNib
 {
-	
+	NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
 }// end - (void) awakeFromNib
 
 #pragma mark - delegate
@@ -40,5 +52,12 @@
 #pragma mark - messages
 #pragma mark - private
 #pragma mark - C functions
+static
+void uncaughtExceptionHandler(NSException *exception)
+{
+    NSLog(@"Exception Name %@, Reason %@", exception.name, exception.reason);
+	NSLog(@"Exception Description %@", exception.description);
+    NSLog(@"Exception Call Stack Symbols %@", exception.callStackSymbols);
+}// end void uncaughtExceptionHandler(NSException *exception)
 
 @end

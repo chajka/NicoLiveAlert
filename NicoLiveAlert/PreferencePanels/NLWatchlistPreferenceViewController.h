9e78d28567b44b40f4f337623162a395986f87d6
Я Чайка <mellanie335@gmail.com>
2014-07-03 01:04:14 +0900

    Add NLWatchlistPrefview controller class and its interface.

---
+++ NicoLiveAlert/PreferencePanels/NLWatchlistPreferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLWatchlistPreferenceViewController.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLWatchlistPreferenceViewController : NSViewController
+
+@end

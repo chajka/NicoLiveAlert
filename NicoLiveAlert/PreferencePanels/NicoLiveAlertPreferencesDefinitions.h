28f8a7a830d339cb7c4d9062c1b94fbc83daa989
Я Чайка <mellanie335@gmail.com>
2014-07-03 04:01:10 +0900

    Add application and preferences definition files.

---
+++ NicoLiveAlert/PreferencePanels/NicoLiveAlertPreferencesDefinitions.h
@@ -0,0 +1,17 @@
+//
+//  NicoLiveAlertPreferencesDefinitions.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#ifndef NicoLiveAlert_NicoLiveAlertPreferencesDefinitions_h
+#define NicoLiveAlert_NicoLiveAlertPreferencesDefinitions_h
+#pragma mark - General
+#pragma mark - Watchlist
+#pragma mark - Notify
+#pragma mark - Account
+
+
+#endif

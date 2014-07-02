43e2d9d8eee49169e393fa0313a6bb08d4b53f52
Я Чайка <mellanie335@gmail.com>
2014-07-03 01:16:04 +0900

    Add NLAccountPrefview controller class and its interface.

---
+++ NicoLiveAlert/PreferencePanels/NLAccountPreferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLAccountPreferenceViewController.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLAccountPreferenceViewController : NSViewController
+
+@end

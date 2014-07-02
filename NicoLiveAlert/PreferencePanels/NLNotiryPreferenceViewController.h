5f57b637256b819fbfdbc267c62badbcca1c773d
Я Чайка <mellanie335@gmail.com>
2014-07-03 01:09:19 +0900

    Add NLNotifylPrefview controller class and its interface.

---
+++ NicoLiveAlert/PreferencePanels/NLNotiryPreferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLNotiryPreferenceViewController.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLNotiryPreferenceViewController : NSViewController
+
+@end
---
+++ NicoLiveAlert/PreferencePanels/NLNotiryPreferenceViewController.mm
@@ -0,0 +1,41 @@
+//
+//  NLNotiryPreferenceViewController.mm
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import "NLNotiryPreferenceViewController.h"
+
+@interface NLNotiryPreferenceViewController ()
+
+@end
+
+@implementation NLNotiryPreferenceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+#pragma mark - MASPreferencesViewController
+- (NSString *)identifier
+{
+    return @"IdentifierName";
+}// end - (NSString *)identifier
+
+- (NSImage *)toolbarItemImage
+{
+    return nil;	// NSImage
+}// end - (NSImage *)toolbarItemImage
+
+- (NSString *)toolbarItemLabel
+{
+    return @"ToolbarTitle";
+}// end - (NSString *)toolbarItemLabel
+@end

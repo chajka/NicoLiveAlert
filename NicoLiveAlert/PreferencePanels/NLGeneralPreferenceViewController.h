c0ff32f06d4d16e8c2775f53d88a1e5ff39c0d78
Я Чайка <mellanie335@gmail.com>
2014-07-03 00:43:47 +0900

    Add NLGeneralPrefview controller class and its interface

---
+++ NicoLiveAlert/PreferencePanels/NLGeneralPreferenceViewController.h
@@ -0,0 +1,14 @@
+//
+//  NLGeneralPreferenceViewController.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MASPreferencesViewController.h"
+
+@interface NLGeneralPreferenceViewController : NSViewController <MASPreferencesViewController>
+
+@end
